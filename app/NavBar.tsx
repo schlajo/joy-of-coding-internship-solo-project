1f8c888d26a106f47ef878cfd67a425462206bca
schlajo <schlajo1981@gmail.com>
2024-12-06 22:41:56 -0600

    Styled active link in NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,23 +1,33 @@
-import Link from 'next/link'
-import React from 'react'
+'use client';
 
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+import classnames from 'classnames';
 const Navbar = () => {
-  const links = [
-      { label: 'Dashboard', href: '/' },
-      { label: 'Issues', href: '/issues' },
-  ]
-  
-      return (
-      <nav className = 'flex space border-b mb-5 px-5 h-14 items-center'>
-            <ul className = 'flex space-x-6'>
-                  {links.map(link => 
-                        <Link 
-                              key={link.href}
-                              className="text-zinc-500 hover:text-zinc-800 transition-colors" 
-                              href={link.href}>{link.label}</Link>)}
-            </ul>      
-      </nav>
-  )
-}
+  const currentPath = usePathname();
 
-export default Navbar
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space border-b mb-5 px-5 h-14 items-center">
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classnames({
+                  'text-zinc-900' : link.href === currentPath,                   
+                  'text-zinc-500' : link.href !== currentPath,
+                  'hover:text-zinc-800 transition-colors': true
+            })}
+            href={link.href}>{link.label}</Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
