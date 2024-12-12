2077d918b1e9507e688189b3197e347d3fa0f6a9
schlajo <schlajo1981@gmail.com>
2024-12-11 19:34:36 -0600

    Handle Errors

--- app/tasks/new/page.tsx
+++ app/tasks/new/page.tsx
@@ -1,12 +1,12 @@
-'use client'
+"use client";
 
-import { Button, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
-import { useForm, Controller } from 'react-hook-form';
-import axios from 'axios';
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import { useRouter } from 'next/navigation';
-
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface TaskForm {
   title: string;
@@ -15,24 +15,39 @@ interface TaskForm {
 
 const NewTaskPage = () => {
   const router = useRouter();
-  const {register, control, handleSubmit} = useForm<TaskForm>();
+  const { register, control, handleSubmit } = useForm<TaskForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form 
-      className='max-w-xl space-y-3' 
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/tasks', data);
-        router.push('/tasks');
-      })}>
-      <TextField.Root placeholder='Title' {...register('title')}/>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field} ) => <SimpleMDE placeholder='Description' {...field}/>}
-        />    
-      <Button>Submit New Task</Button>
-    </form>
-  )
-}
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className='mb-5'>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/tasks", data);
+            router.push("/tasks");
+          } catch (error) {
+            setError("An unexprected error occurred.");
+          }
+        })}
+      >
+        <TextField.Root placeholder="Title" {...register("title")} />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <Button>Submit New Task</Button>
+      </form>
+    </div>
+  );
+};
 
-export default NewTaskPage
+export default NewTaskPage;
